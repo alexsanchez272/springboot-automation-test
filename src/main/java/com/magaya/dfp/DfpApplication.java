15407ef7fbcfdde41eeffedc6b6bd1353dc894f2
Manuel Sanchez <axtech272@gmail.com>
2022-11-16 12:09:22 +0100

    Established base package component scan

--- src/main/java/com/magaya/dfp/DfpApplication.java
+++ src/main/java/com/magaya/dfp/DfpApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DfpApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DfpApplication.class, args);
 	}
 
